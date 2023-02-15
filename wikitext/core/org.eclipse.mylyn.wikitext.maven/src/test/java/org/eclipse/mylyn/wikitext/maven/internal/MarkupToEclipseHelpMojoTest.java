eclipse-mylyn/org.eclipse.mylyn

commit 803292b246c65c115333d12b2730a3b9759bb397

Replace deprecated Guava Files.toString
With Java 11's Files.readString().

Change-Id: Ie7e03ec01346eb92436455746d281ecb28277357

wikitext/core/org.eclipse.mylyn.wikitext.maven/src/main/java/org/eclipse/mylyn/wikitext/maven/internal/MarkupToEclipseHelpMojo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2021 Tasktop Technologies and others.
+ * Copyright (c) 2013, 2022 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -47,8 +48,6 @@ import org.eclipse.mylyn.wikitext.splitter.SplittingMarkupToEclipseToc;
 import org.eclipse.mylyn.wikitext.splitter.SplittingOutlineParser;
 import org.eclipse.mylyn.wikitext.splitter.SplittingStrategy;
 import org.eclipse.mylyn.wikitext.util.ServiceLocator;
-
-import com.google.common.io.Files;
 
 @Mojo(name = "eclipse-help", defaultPhase = LifecyclePhase.COMPILE)
 public class MarkupToEclipseHelpMojo extends AbstractMojo {
@@ -237,7 +236,7 @@ public class MarkupToEclipseHelpMojo extends AbstractMojo {
 		ensureFolderExists("target folder", targetFolder, true);
 		File targetFile = new File(targetFolder, sourceFile.getName());
 		try {
-			Files.copy(sourceFile, targetFile);
+			Files.copy(sourceFile.toPath(), targetFile.toPath());
 		} catch (IOException e) {
 			throw new BuildFailureException(
 					format("Cannot copy {0} to {1}: {2}", sourceFile, targetFile, e.getMessage()), e);
@@ -460,7 +459,7 @@ public class MarkupToEclipseHelpMojo extends AbstractMojo {
 
 	protected String readFully(File inputFile) {
 		try {
-			return Files.toString(inputFile, Charset.forName(sourceEncoding));
+			return Files.readString(inputFile.toPath(), Charset.forName(sourceEncoding));
 		} catch (IOException e) {
 			throw new BuildFailureException(format("Cannot read source file {0}: {1}", inputFile, e.getMessage()), e);
 		}

wikitext/core/org.eclipse.mylyn.wikitext.maven/src/test/java/org/eclipse/mylyn/wikitext/maven/internal/MarkupToEclipseHelpMojoTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Tasktop Technologies and others.
+ * Copyright (c) 2013, 2022 Tasktop Technologies and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -47,8 +48,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.ArgumentCaptor;
-
-import com.google.common.io.Files;
 
 public class MarkupToEclipseHelpMojoTest {
 
@@ -270,7 +269,7 @@ public class MarkupToEclipseHelpMojoTest {
 		assertTrue(file.toString(), file.exists());
 		assertTrue(file.toString(), file.isFile());
 		try {
-			String content = Files.toString(file, StandardCharsets.UTF_8);
+			String content = Files.readString(file.toPath(), StandardCharsets.UTF_8);
 			assertTrue(String.format("expected %s but got %s", expectedContent, content),
 					content.contains(expectedContent));
 		} catch (IOException e) {

wikitext/core/org.eclipse.mylyn.wikitext.mediawiki.ant/src/test/java/org/eclipse/mylyn/wikitext/mediawiki/ant/internal/tasks/WikiToDocTaskTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2021 David Green and others.
+ * Copyright (c) 2007, 2022 David Green and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,8 +29,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
-import com.google.common.io.Files;
 
 public class WikiToDocTaskTest {
 
@@ -94,7 +93,7 @@ public class WikiToDocTaskTest {
 		task.execute();
 
 		File result = task.computeHtmlOutputFile(path);
-		String content = Files.toString(result, StandardCharsets.UTF_8);
+		String content = Files.readString(result.toPath(), StandardCharsets.UTF_8);
 		assertTrue(content.contains(
 				"<p>Link to <a href=\"#IActivatable\" title=\"GEF/GEF4/Common#IActivatable\">IActivatable</a> is here</p>"));
 	}
@@ -137,7 +136,7 @@ public class WikiToDocTaskTest {
 		assertEquals("'Installation.html' page exists", true, installationPage.exists());
 		File taskListPage = new File(mainPage.getParentFile(), "Task-List.html");
 		assertEquals("'Task-List.html' page exists", true, taskListPage.exists());
-		String content = Files.toString(taskListPage, StandardCharsets.UTF_8);
+		String content = Files.readString(taskListPage.toPath(), StandardCharsets.UTF_8);
 		assertTrue(content.contains(
 				"<a href=\"Installation.html#Recommended_GTK_Setup_for_KDE\" title=\"Mylyn/FAQ#Recommended_GTK_Setup_for_KDE\">Recommended GTK Setup for KDE</a> section."));
 	}

eclipse-mylyn/org.eclipse.mylyn.docs

commit 7bdd9453ace95e17c13e321e1955e7cb95cd0ad7

Fix regression in wikitext maven plugin when copying non-markup files
The wikitext maven plugin exists to facilitate creating HTML
documentation from eclipse help source files. A regression was
introduced with commit
803292b246c65c115333d12b2730a3b9759bb397 that switched out the Guava
Files.copy method for the java.nio.file.Files.copy method.

The semantics of those 2 methods are different. While Guava's copy
method will always replace/overwrite the target file if it already
exists, the java.nio.file.Files.copy method does not do that by
default. The optional StandardCopyOption.REPLACE_EXISTING must
be provided as a parameter to allow for the same behavior. This
change is important if the workspace is not clean when the
wikitext.maven mojo is executed.

wikitext/core/org.eclipse.mylyn.wikitext.maven/src/main/java/org/eclipse/mylyn/wikitext/maven/internal/MarkupToEclipseHelpMojo.java
@@ -25,6 +25,7 @@ import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -236,7 +237,7 @@ public class MarkupToEclipseHelpMojo extends AbstractMojo {
 		ensureFolderExists("target folder", targetFolder, true);
 		File targetFile = new File(targetFolder, sourceFile.getName());
 		try {
-			Files.copy(sourceFile.toPath(), targetFile.toPath());
+			Files.copy(sourceFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			throw new BuildFailureException(
 					format("Cannot copy {0} to {1}: {2}", sourceFile, targetFile, e.getMessage()), e);

wikitext/core/org.eclipse.mylyn.wikitext.maven/src/test/java/org/eclipse/mylyn/wikitext/maven/internal/MarkupToEclipseHelpMojoTest.java
@@ -143,6 +143,18 @@ public class MarkupToEclipseHelpMojoTest {
 	}
 
 	@Test
+	public void processNonMarkupFileOverwritesTargetFile() throws IOException {
+		URL resource = MarkupToEclipseHelpMojoTest.class.getResource("/test.textile");
+		File file = new File(resource.getPath());
+
+		markupToEclipseHelp.process(file, "", null);
+		markupToEclipseHelp.process(file, "", null);
+
+		assertTrue(computeOutputFile("test.textile").exists());
+		assertHasContent("test.textile", Files.readString(file.toPath(), StandardCharsets.UTF_8));
+	}
+
+	@Test
 	public void configureStylesheetUrls() {
 		markupToEclipseHelp.stylesheetUrls = Arrays.asList("test/foo.css", "bar.css");
 		HtmlDocumentBuilder builder = mock(HtmlDocumentBuilder.class);